df055ceac4a697ab1326bafd07fe13b31dbf9f84
Daniel Ancines <daniel.ancines@gmail.com>
2020-04-15 11:57:04 -0300

    Nao carregava os acessos e grupos no cadastro de usuarios

--- src/app/manager/users/user/user.component.ts
+++ src/app/manager/users/user/user.component.ts
@@ -297,6 +297,7 @@ export class UserComponent extends ComponentBase implements OnInit, AfterViewIni
       '?orderBy=name'
     );
     this.groupsDataSource.addColumns(['select', 'name', 'description']);
+    this.groupsDataSource.refresh();
 
     this.groupsDataSource.data.subscribe(groups => {
       this.updateSelectedGroups(groups);
@@ -312,6 +313,7 @@ export class UserComponent extends ComponentBase implements OnInit, AfterViewIni
     );
 
     this.rolesDataSource.addColumns(['select', 'name', 'description']);
+    this.rolesDataSource.refresh();
 
     this.rolesDataSource.data.subscribe(roles => {
       this.updateSelectedRoles(roles);
